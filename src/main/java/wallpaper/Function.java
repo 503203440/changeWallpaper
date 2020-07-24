fd72242e6b6ecb840e2f27b1f39993d03809e945
YX <503203440@qq.com>
2020-07-24 16:36:03 +0800

    *

--- src/main/java/wallpaper/Function.java
+++ src/main/java/wallpaper/Function.java
@@ -4,8 +4,17 @@ import com.sun.jna.platform.win32.Advapi32Util;
 import com.sun.jna.platform.win32.WinReg;
 import jna.User32;
 
+/**
+ * 修改壁纸参考https://blog.csdn.net/iteye_878/article/details/82613410
+ * java使用JNA参考
+ * https://www.cnblogs.com/lanxuezaipiao/p/3635556.html
+ * https://blog.csdn.net/qq_33056389/article/details/98966261
+ */
 public class Function {
 
+    /**
+     * @param imgPath 图片地址，注意不要有中文，否则写入注册表容易出现乱码
+     */
     public static void changeWallpaper(String imgPath) {
 
         Advapi32Util.registrySetStringValue(WinReg.HKEY_CURRENT_USER,
